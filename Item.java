b8e4be7d1fe7c42c3625f023052ce9c8a1ebaba2
hasnanfiqih1 <47843372+hasnanfiqih1@users.noreply.github.com>
2019-02-21 16:33:09 +0700

    modul 1
    modul 1

---
+++ Item.java
@@ -0,0 +1,58 @@
+
+
+/**
+ * Write a description of class Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Item
+{
+   protected int id;
+   protected String name;
+   protected int stock;
+   protected int price;
+   protected String category;
+   
+   public  void Item(){
+    }
+    
+   public int getId(){
+       return 0;
+    }
+   
+   public String getName(){
+       return "";
+    }
+    
+   public int getStock(){
+       return 0;
+    }
+    
+    public int getPrice(){
+        return 0;
+    }
+    
+    public String getCategory(){
+        return "";
+    }
+    
+    public int setId(int id){
+        return 0;
+    }
+    
+    public String setName(String id){
+        return "";
+    }
+    public int setStock(int id){
+        return 0;
+    }
+    public int setPrice(int id){
+        return 0;
+    }
+    public String setCategory(String id){
+        return "";
+    }
+    
+    
+}
---
+++ JStore.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class JStore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JStore
+{
+    public void JStore(){
+    }
+}
